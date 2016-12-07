45ee963acb7fdb2cb4453c0ecf4d82886e9978e7
Student <student@macbook-pro-1187-ormsby.local>
2016-12-06 22:27:31 -0500

    Got mario working needed to use the model on haiku to make it work though.

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -12,8 +12,38 @@
 
 // Add your code below
 
-func levelCost(heights: [Int], maxJump: Int) -> Int {
-    
-    
+func levelCost(heights: [Int], maxJump : Int) -> Int {
+
+    var energyUse : Int = 0
+
+for i in 0...heights.count - 2 {
+
+let currentPlatformHeight = heights[i]
+
+let nextPlatformHeight = heights[i + 1]
+
+let heightDifference = abs(currentPlatformHeight - nextPlatformHeight)
+
+    if heightDifference == 0 {
+
+energyUse += 1
+
+    } else {
+
+        if heightDifference > maxJump {
+
+            return -1
+        } else {
+
+energyUse += heightDifference * 2
 }
+}
+}
+return energyUse
+}
+
+
+
+
+
 
